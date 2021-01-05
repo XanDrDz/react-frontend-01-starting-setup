226e5b3e343577ce50372407a3697e0d29adadea
XanDrDz <64421185+XanDrDz@users.noreply.github.com>
2021-01-05 17:45:05 +0500

    NAV

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 
 import Users from './user/pages/Users';
-import NewPlace from './places/pages/NewPlace'
+import NewPlace from './places/pages/NewPlace';
+import MainNavigation from './shared/components/Navigation/MainNavigation';
 
 function App() {
   return (
   <Router>
+    <MainNavigation />
+    <main>
     <Switch>
     <Route path="/" exact>
        <Users />
@@ -16,6 +19,7 @@ function App() {
     </Route>
     <Redirect to="/" />
     </Switch>
+    </main>
   </Router>
   );
 }
