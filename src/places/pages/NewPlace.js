f91c48a944a4a5a2732d5dbcb3d3569fdc4cc3fc
XanDrDz <64421185+XanDrDz@users.noreply.github.com>
2021-01-12 17:28:06 +0500

    Validation

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -4,10 +4,18 @@ import Input from '../../shared/components/FormElements/Input';
 import './NewPlace.css';
 
 const NewPlace = () => {
-    return <form className="place-form">
+    return (
+    <form className="place-form">
         <label></label>
-        <Input element="input" type="text" label="Title" />
-    </form>;
+        <Input 
+        element="input" 
+        type="text" 
+        label="Title" 
+        validators={[]} 
+        errorText="Please enter a valid title."
+         />
+    </form>
+    );
 };
 
 export default NewPlace;
